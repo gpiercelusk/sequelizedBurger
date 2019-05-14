113e02451e1c94b1720daf32ef76ed399e588bda
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-05-14 16:21:34 -0400

    added .gitignore, deleted connect.js and orm.js updated models and sql files

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,19 @@
-var orm = require("../config/orm.js");
+module.exports = function (sequelize, DataTypes) {
+  var burger = sequelize.define("burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defauletValue: false
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
+    }
+  });
+  return burger;
+
 };
 
-module.exports = burger;
